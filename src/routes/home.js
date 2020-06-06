ae53b3511c40aed3cac10ed1de4f52e77796846e
davidkvara <davekvaratskhelia@gmail.com>
2020-06-06 22:02:25 +0400

    remove redundant character

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div style={{ maxWidth: 880, margin: "0 auto" }}>
       <div className="row">
-        <Tile name="მობილური" goTo="/mobile" />
-        <Tile name="კომუნალური" goTo="/utility" />
+        <Tile name="მობილური" goTo="mobile" />
+        <Tile name="კომუნალური" goTo="utility" />
       </div>
       <div className="row">
-        <Tile name="ფინანსური მომსახურება" goTo="/finances" />
-        <Tile name="ქველმოქმედება" goTo="/charity" />
+        <Tile name="ფინანსური მომსახურება" goTo="finances" />
+        <Tile name="ქველმოქმედება" goTo="charity" />
       </div>
     </div>
   );
