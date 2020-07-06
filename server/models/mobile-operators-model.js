5436219fe13ac62434ae8db49156375f82902cd4
Nino Keldishvili <ninokeldishvili@gmail.com>
2020-07-07 00:28:15 +0400

    Add Mobile Balance fill functionality, Data saved to DB

--- server/models/mobile-operators-model.js
+++ server/models/mobile-operators-model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let MobileOperator = new Schema({
-    mobile_number: {
+    number: {
         type: String
     },
     amount: {
--- src/routes/mobile.js
+++ src/routes/mobile.js
@@ -3,6 +3,7 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 import ComissionRow from "../components/ComissionRow";
 import ErrorMessage from "../components/ErrorMessage";
+import axios from 'axios';
 
 class Mobile extends React.Component {
   state = {
@@ -36,10 +37,28 @@ class Mobile extends React.Component {
     return !(number.length === 9 && !(amount > 100 || amount < 1));
   };
 
-  handleClick = () => {
-    const { comission, amount } = this.state;
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { comission, amount, number } = this.state;
+
+    const fillBalance = {
+      number,
+      amount,
+      comission,
+    }
+
+
+    axios.post('http://localhost:4000/fill-mobile', fillBalance)
+         .then(res => console.log(res.data));
+
     alert(`Your Balance is filled with ${amount - comission} GEL, Thank You!`);
-  };
+
+    this.setState({
+      number:'',
+      amount:'',
+      comission: 0,
+    })
+}
 
   render() {
     const { number, amount, comission } = this.state;
@@ -60,7 +79,7 @@ class Mobile extends React.Component {
         <ComissionRow value={comission} />
         <Button
           isDisabled={this.disableButton()}
-          onClick={this.handleClick}
+          onClick={this.onSubmit}
           children="Fill Balance"
         />
         {amount && (amount > 100 || amount < 1) && (
